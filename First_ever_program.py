45c6ad27d32a56135aeac1d7b864aff45cca8c1e
locke21 <100035903+locke21@users.noreply.github.com>
2022-03-16 21:50:52 -0400

    Update First_ever_program.py

--- First_ever_program.py
+++ First_ever_program.py
@@ -1,16 +1,22 @@
+# --- First Assignment from Dr. Chuck's "Python for Everybody" Specialization. First day learning Python --- #
+# --- Objective: Load text file and count how many unique words in the file --- #
+# --- The file works off the auto-grader from Coursera so the outputs/Variables makes more sense there --- #
 
+# name = "words.txt"
 name = input('enter file:')
-handle = open(name)
+file = open(name)
 
-counts = dict()
-for line in handle:
-    words = line.split()
-    for word in words:
-        counts[word] = counts.get(word, 0) + 1
+all_words = dict()
+for line in file:
+    for word in line.split():
+        all_words[word] = all_words.get(word, 0) + 1
 
-bigcount = None
-bigword = None
-for word, count in counts.items():
-    if bigcount is None or count > bigcount:
-        bigword = word
-        bigcount = count
+times_word_used = None
+most_used_word = None
+for key, value in all_words.items():
+    if times_word_used is None or value > times_word_used:
+        most_used_word = key
+        times_word_used = value
+
+print(f'The word "{most_used_word}" was used {times_word_used} times, '
+      f'making it the most used word out of {len(all_words)} unique words. ')
