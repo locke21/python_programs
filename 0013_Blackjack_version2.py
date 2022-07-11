00edde37a5adf496dbdc5f357b55a9e52a9178d9
Loc Truong <100035903+locke21@users.noreply.github.com>
2022-07-10 20:34:22 -0400

    Update 0013_Blackjack_version2.py

--- 0013_Blackjack_version2.py
+++ 0013_Blackjack_version2.py
@@ -22,7 +22,7 @@ def change_face_card(card):
 
 
 def calc_total(hand):
-    aces = sum(1 for ace in hand if ace == 11)
+    aces = sum(1 for card in hand if card == 11)
     total = sum(card for card in hand)
     while total > 21:
         if aces >= 1:
@@ -35,16 +35,13 @@ def calc_total(hand):
 
 def check_dealers_hand(dealer_hand):
     if calc_total(hand=dealer_hand) > calc_total(hand=player_hand):
-        print(f'Players hand: {player_hand_displayed}. ')
-        print(f'Dealers hand: {dealer_hand_displayed}. \n')
-        print(f'You lose with a {calc_total(hand=player_hand)}. Dealer has {calc_total(hand=dealer_hand)}! ')
+        print(f"Dealer: {len(dealer_hand)} cards || Dealer hand: {(', '.join(map(str, dealer_hand_displayed)))} "
+              f"(total: {calc_total(hand=dealer_hand)})")
+        print(f"Player: {len(player_hand)} cards || Your   hand: {(', '.join(map(str, player_hand_displayed)))} "
+              f"(total: {calc_total(hand=player_hand)})\n")
+        print(f'You lose with a {calc_total(hand=player_hand)}! Dealer has {calc_total(hand=dealer_hand)}! ')
         quit()
     while calc_total(dealer_hand) < 17:
-        new_card = random.choice(card_deck)
-        dealer_hand.append(new_card)
-        new_card = change_face_card(card=new_card)
-        dealer_hand_displayed.append(new_card)
-    if calc_total(dealer_hand) < calc_total(player_hand):
         new_card = random.choice(card_deck)
         dealer_hand.append(new_card)
         new_card = change_face_card(card=new_card)
@@ -53,16 +50,18 @@ def check_dealers_hand(dealer_hand):
 
 
 def check_winner():
-    print(f'Players hand: {player_hand_displayed}. ')
-    print(f'Dealers hand: {dealer_hand_displayed}. \n')
+    print(f"Dealer: {len(dealer_hand)} cards || Dealer hand: {(', '.join(map(str, dealer_hand_displayed)))} "
+          f"(total: {calc_total(hand=dealer_hand)})")
+    print(f"Player: {len(player_hand)} cards || Your   hand: {(', '.join(map(str, player_hand_displayed)))} "
+          f"(total: {calc_total(hand=player_hand)})\n")
     if calc_total(hand=player_hand) > calc_total(hand=dealer_hand):
-        print(f'You win with a {calc_total(hand=player_hand)}. Dealer has {calc_total(hand=dealer_hand)}! ')
+        print(f'You win with a {calc_total(hand=player_hand)}! Dealer has {calc_total(hand=dealer_hand)}! ')
     elif calc_total(hand=dealer_hand) > 21:
-        print(f'You win with a {calc_total(hand=player_hand)}. Dealer went over with a {calc_total(hand=dealer_hand)}! ')
+        print(f'You win with a {calc_total(hand=player_hand)}! Dealer went over with a {calc_total(hand=dealer_hand)}!')
     elif calc_total(hand=player_hand) < calc_total(hand=dealer_hand):
-        print(f'You lose with a {calc_total(hand=player_hand)}. Dealer has {calc_total(hand=dealer_hand)}! ')
+        print(f'You lose with a {calc_total(hand=player_hand)}! Dealer has {calc_total(hand=dealer_hand)}! ')
     else:
-        print(f'Tie with a {calc_total(hand=player_hand)}. Dealer has {calc_total(hand=dealer_hand)}! ')
+        print(f'You both tied with {calc_total(hand=player_hand)}!')
 
 for card in player_hand:
     player_hand_displayed.append(change_face_card(card))
@@ -70,12 +69,12 @@ for card in dealer_hand:
     dealer_hand_displayed.append(change_face_card(card))
 
 clear()
-print(f"Dealer: {len(dealer_hand)} cards - Dealer shows: {dealer_hand_displayed[0]}")
-print(f"Player: {len(player_hand)} cards - Total: {calc_total(hand=player_hand)} "
-      f"|| Your hand: {(', '.join(map(str, player_hand_displayed)))}. ")
+print(f"Dealer: {len(dealer_hand)} cards || Dealer show: {dealer_hand_displayed[0]}")
+print(f"Player: {len(player_hand)} cards || Your   hand: {(', '.join(map(str, player_hand_displayed)))} "
+      f"(total: {calc_total(hand=player_hand)})\n")
 
 while playing_blackjack:
-    deal_card = input('\nWould you like to Hit(1 or H) or Stay (2 or S)? ')
+    deal_card = input('Would you like to Hit(1 or H) or Stay (2 or S)? ')
     clear()
     if deal_card == "1" or deal_card.lower() == "h":
         new_card = random.choice(card_deck)
@@ -83,11 +82,13 @@ while playing_blackjack:
         new_card = change_face_card(card=new_card)
         player_hand_displayed.append(new_card)
         print(f"Card dealt was: {new_card}.")
-        print(f"Dealer: {len(dealer_hand)} cards - Dealer shows: {dealer_hand_displayed[0]}")
-        print(f"Player: {len(player_hand)} cards - Total: {calc_total(hand=player_hand)} "
-              f"|| Your hand: {(', '.join(map(str, player_hand_displayed)))}. ")
+        print(f"Dealer: {len(dealer_hand)} cards || Dealer show: {dealer_hand_displayed[0]} ")
+        print(f"Player: {len(player_hand)} cards || Your   hand: {(', '.join(map(str, player_hand_displayed)))} "
+              f"(total: {calc_total(hand=player_hand)})\n")
         if calc_total(hand=player_hand) > 21:
-            print(f'Sorry you lose with a {calc_total(hand=player_hand)}. Dealer has {calc_total(hand=dealer_hand)}! ')
+            print(f'Sorry you went over with a {calc_total(hand=player_hand)}! '
+                  f"Dealer wins with {(', '.join(map(str, dealer_hand_displayed)))} "
+                  f"(total: {calc_total(hand=dealer_hand)})! ")
             quit()
     else:
         playing_blackjack = False
